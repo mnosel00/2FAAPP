24aaf9b69f01cd7020e8f570bb23d9f719f722b6
Mateusz Nosel <mateusz.nosel@student.pk.edu.pl>
2025-10-15 21:07:54 +0200

    edited Authres model

--- BackEnd/2FA_Backend/2FA_Backend.Domain/DTOs/AuthResult.cs
+++ BackEnd/2FA_Backend/2FA_Backend.Domain/DTOs/AuthResult.cs
@@ -8,13 +8,13 @@ namespace _2FA_Backend.Domain.DTOs
 {
     public class AuthResult
     {
-        public bool Success { get; set; }
-        public string Token { get; set; }
-        public bool Requires2FA { get; set; }
+        public bool Success { get; set; } = false;
+        public bool TwoFactorRequired { get; set; } = false;
         public string UserId { get; set; }
-        public string SetupKey { get; set; }
-        public  string QrCodeUrl { get; set; }
-        public IEnumerable<string> Errors { get; set; }
+        public string Token { get; set; } 
+        public string SetupKey { get; set; } 
+        public string QrCodeUri { get; set; }
+        public IEnumerable<string> Errors { get; set; } = Enumerable.Empty<string>();
 
     }
 }
