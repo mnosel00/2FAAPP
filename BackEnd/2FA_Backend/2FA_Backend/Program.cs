5d6884c26288a8fe19dcb058540f4d72c3060df0
Mateusz Nosel <mateusz.nosel@student.pk.edu.pl>
2025-10-15 22:05:39 +0200

    conf Program

--- BackEnd/2FA_Backend/2FA_Backend/Program.cs
+++ BackEnd/2FA_Backend/2FA_Backend/Program.cs
@@ -1,3 +1,12 @@
+using _2FA_Backend.Application.Interfaces;
+using _2FA_Backend.Application.Services;
+using _2FA_Backend.Domain.Interfaces;
+using _2FA_Backend.Infastructure.Data;
+using _2FA_Backend.Infastructure.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,16 +15,41 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Tokens.AuthenticatorTokenProvider = TokenOptions.DefaultAuthenticatorProvider;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>() 
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .WithOrigins("http://localhost:4200");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("CorsPolicy");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
