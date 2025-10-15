ea51d20f7614fe669e82cfb8a9e289ce5117ec14
Mateusz Nosel <mateusz.nosel@student.pk.edu.pl>
2025-10-15 21:01:44 +0200

    authService

---
+++ BackEnd/2FA_Backend/2FA_Backend.Application/Services/AuthService.cs
@@ -0,0 +1,36 @@
+﻿using _2FA_Backend.Application.Interfaces;
+using _2FA_Backend.Domain.DTOs;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2FA_Backend.Application.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthService(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public Task<AuthResult> LoginUserAsync(LoginModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthResult> RegisterUserAsync(RegisterModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthResult> Verify2FAAsync(string userId, string code)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
