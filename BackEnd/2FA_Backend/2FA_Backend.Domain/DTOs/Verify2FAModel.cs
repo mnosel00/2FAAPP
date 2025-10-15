b297ac40d92ab8134e1a89e94532971d2f1587d9
Mateusz Nosel <mateusz.nosel@student.pk.edu.pl>
2025-10-15 21:14:34 +0200

    user Repo

---
+++ BackEnd/2FA_Backend/2FA_Backend.Domain/DTOs/Verify2FAModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2FA_Backend.Domain.DTOs
+{
+    public class Verify2FAModel
+    {
+        public string UserId { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ BackEnd/2FA_Backend/2FA_Backend.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2FA_Backend.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IdentityUser> FindByEmailAsync(string email);
+        Task<bool> CheckPasswordAsync(IdentityUser user, string password);
+        Task<IdentityResult> CreateAsync(IdentityUser user, string password);
+        Task<string> GetAuthenticatorKeyAsync(IdentityUser user);
+        Task<IdentityResult> ResetAuthenticatorKeyAsync(IdentityUser user);
+        Task<bool> GetTwoFactorEnabledAsync(IdentityUser user);
+        Task<IdentityResult> SetTwoFactorEnabledAsync(IdentityUser user, bool enabled);
+        Task<IdentityUser> FindByIdAsync(string userId);
+        Task<bool> VerifyTwoFactorTokenAsync(IdentityUser user, string tokenProvider, string code);
+    }
+}
